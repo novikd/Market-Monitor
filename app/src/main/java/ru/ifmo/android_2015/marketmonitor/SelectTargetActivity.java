290d8975c75beffe321d31d1811814b82389e7af
Dmitry Novik <d1mas1k4@yandex.ru>
2015-11-03 21:21:43 +0300

    Initial commit

---
+++ app/src/main/java/ru/ifmo/android_2015/marketmonitor/SelectTargetActivity.java
@@ -0,0 +1,13 @@
+package ru.ifmo.android_2015.marketmonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectTargetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_target);
+    }
+}
