5a850447df5e22daef1ff0ea00bf369e9160b142
rthakohov <ruslan.thakokhov@gmail.com>
2015-11-28 22:55:33 +0300

    minor changes

--- app/src/main/java/ru/ifmo/android_2015/marketmonitor/AddTargetActivity.java
+++ app/src/main/java/ru/ifmo/android_2015/marketmonitor/AddTargetActivity.java
@@ -4,26 +4,18 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 import java.util.List;
-import java.util.Objects;
 
 import db.MarketDB;
-import db.MarketMonitorDBHelper;
 import list.CategoriesRecyclerAdapter;
 import list.RecyclerDividerDecorator;
 import request.GetCategoriesTask;
